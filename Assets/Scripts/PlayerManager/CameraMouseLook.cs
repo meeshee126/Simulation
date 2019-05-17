07261032df67a552f3d93f86656b9cef2d05d271
Michael Schmidt <schmidtmichael89@hotmail.de>
2019-05-17 15:46:59 +0200

    Added Terrain and NavMesh

---
+++ Assets/Scripts/PlayerManager/CameraMouseLook.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMouseLook : MonoBehaviour
+{
+
+    private Vector2 m_mouseLook;
+    private Vector2 m_smooth;
+
+    [SerializeField]
+    private float m_sensitivity = 5f, m_smoothing = 2f;
+
+    private UnityEngine.GameObject m_player;
+
+    void Start()
+    {
+        m_player = this.transform.parent.gameObject;
+    }
+
+    void Update()
+    {
+        Vector2 mouseVelocity = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseVelocity = Vector2.Scale(mouseVelocity, new Vector2(m_sensitivity * m_smoothing, m_sensitivity * m_smoothing));
+        m_smooth.x = Mathf.Lerp(m_smooth.x, mouseVelocity.x, 1f / m_smoothing);
+        m_smooth.y = Mathf.Lerp(m_smooth.y, mouseVelocity.y, 1f / m_smoothing);
+
+        m_mouseLook += m_smooth;
+
+        //clamp looking up and down
+        m_mouseLook.y = Mathf.Clamp(m_mouseLook.y, -90f, 90f);
+
+        transform.localRotation = Quaternion.AngleAxis(-m_mouseLook.y, Vector3.right);
+        m_player.transform.localRotation = Quaternion.AngleAxis(m_mouseLook.x, m_player.transform.up);
+
+    
+    }
+}
---
+++ Assets/Scripts/PlayerManager/PlayerControler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField]
+    private float m_speed;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float walk = Input.GetAxisRaw("Vertical") * m_speed * Time.deltaTime;
+        float strafe = Input.GetAxisRaw("Horizontal") * m_speed * Time.deltaTime;
+        transform.Translate(strafe, 0, walk);
+        
+    }
+}
